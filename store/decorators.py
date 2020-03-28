d45583280146e018e38d080e906aa807fdc6d1bd
Rob van der Linde <rob@catalyst.net.nz>
2020-03-28 21:26:29 +1300

    Get rid of @named_model decorator as it's cryptic
    Seems a bit un-pythonic to inject a method like this through a decorator on the class
    
    It feels like too much magic that needs to be explained to students, so keep it simple.

--- store/decorators.py
+++
@@ -1,8 +0,0 @@
-def named_model(cls):
-    """ Uses `name` attribute for string serialization of object. """
-
-    def __str__(self):
-        return self.name
-
-    cls.__str__ = __str__
-    return cls
--- store/models.py
+++ store/models.py
@@ -4,8 +4,6 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from timezone_field import TimeZoneField
 
-from .decorators import named_model
-
 
 class Person(models.Model):
     first_name = models.CharField(max_length=254)
@@ -18,17 +16,21 @@ class Person(models.Model):
         return f'{self.first_name} {self.last_name}'
 
 
-@named_model
 class Team(models.Model):
     name = models.CharField(max_length=254, unique=True)
     timezone = TimeZoneField(default='Pacific/Auckland')
     email = models.EmailField()
 
+    def __str__(self):
+        return self.name
 
-@named_model
+
 class Project(models.Model):
     name = models.CharField(max_length=254, unique=True)
     teams = models.ManyToManyField('Team', related_name='projects')
+
+    def __str__(self):
+        return self.name
 
     def get_team(self):
         current_time = timezone.now()
@@ -40,19 +42,22 @@ class Project(models.Model):
             return None
 
 
-@named_model
 class Site(models.Model):
     name = models.CharField(max_length=254, unique=True)
     project = models.ForeignKey('Project', related_name='sites', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
 
-@named_model
+
 class SensorType(models.Model):
     name = models.CharField(max_length=254, unique=True)
     units = models.CharField(max_length=12)
 
+    def __str__(self):
+        return self.name
 
-@named_model
+
 class Sensor(models.Model):
     name = models.CharField(max_length=254)
     site = models.ForeignKey('Site', on_delete=models.CASCADE)
@@ -62,6 +67,9 @@ class Sensor(models.Model):
 
     class Meta:
         unique_together = ('name', 'site')
+
+    def __str__(self):
+        return self.name
 
     @property
     def warnings(self):
